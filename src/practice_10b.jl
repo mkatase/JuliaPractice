3ad7b4b32dfb3ecb074afdc3ca8e4b4eb6d8e523
mkatase <michimoto.katase@gmail.com>
2018-09-24 17:35:50 +0900

    commit on 2018/09/24

--- src/practice_10b.jl
+++ src/practice_10b.jl
@@ -10,5 +10,9 @@ u = ccall((:calc, "libextrac"), Int32, (Int32,), 4)
 println(u)
 
 println("-----")
-v = ccall((:hello, "libextrac"), Cstring, (Cstring,), "Aonther")
+v = ccall((:hello, "libextrac"), Cstring, (Cstring,), "Another")
 println(unsafe_string(v))
+
+println("-----")
+z = ccall((:hello, "libextrac"), Ptr{UInt8}, (Cstring,), "Bnother")
+println(unsafe_string(z))
